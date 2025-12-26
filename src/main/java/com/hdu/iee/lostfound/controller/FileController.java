0a3e98edec77a870a1d9f72cbc248a2d018c87f1
Lff553 <2972638139@qq.com>
2025-12-26 10:37:59 +0800

    Update FileController.java

--- src/main/java/com/hdu/iee/lostfound/controller/FileController.java
+++ src/main/java/com/hdu/iee/lostfound/controller/FileController.java
@@ -16,7 +16,6 @@ import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/files")
-@CrossOrigin(origins = "*")
 public class FileController {
 
     @Value("${app.upload.dir:uploads}")
@@ -50,3 +49,4 @@ public class FileController {
         }
     }
 }
+
